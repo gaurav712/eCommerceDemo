9795b81262eee348e47e3bfb688db2970912d4bc
Gaurav Kumar Yadav <gaurav712@protonmail.com>
2022-09-29 00:36:45 +0530

    add nav params to types to make TS happy

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,3 +1,13 @@
+interface INavigationRoute {
+  params: Record<string, any>;
+}
+
+interface INavigationState {
+  index: number;
+  routes: INavigationRoute[];
+}
+
 export interface INavigation {
   navigate: (path: string, params?: any) => unknown;
+  getState: () => INavigationState;
 }
