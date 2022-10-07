d469f1cf789b8233451705f8551aa2136bd821cd
Gaurav Kumar Yadav <gaurav712@protonmail.com>
2022-10-07 10:23:52 +0530

    add splash

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StatusBar } from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 import { Provider } from 'react-redux';
 import { Colors } from './app/config';
 import AppWithNavigation from './app/navigation';
@@ -8,6 +9,10 @@ import store from './app/store';
 
 const App = () => {
   axios.defaults.baseURL = 'http://172.16.146.192:3000/api';
+
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
 
   return (
     <Provider store={store}>
